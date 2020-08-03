e5cca0e9d8750c8b85c6ff474fd4cae57f1123fd
Dan Ilea <doflada@gmail.com>
2020-08-03 16:35:13 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require('cors');
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : 'postgresql-polished-97957',
+    user : 'dan',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {  res.send('it is working!!!') });
+
+app.post('/signin', signin.handleSignin(knex, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, knex, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, knex) });
+app.put('/image', (req, res) => { image.handleImage(req, res, knex) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is run on port ${process.env.PORT}`);
+});
